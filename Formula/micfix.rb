3c3a6ff58e9acf22047158ad7bb33b4c69d5a0e5
LimxBot <84507948+LimxBot@users.noreply.github.com>
2021-05-20 12:18:15 +0000

    micfix: update 1.1.3 bottle.

--- Formula/micfix.rb
+++ Formula/micfix.rb
@@ -8,7 +8,7 @@ class Micfix < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/winglim/taps"
-    sha256 cellar: :any_skip_relocation, catalina: "34abd1ae69816ffe499328af2c7f6d5d923cbcc9935aac5b2c2bf3dae144d5ce"
+    sha256 cellar: :any_skip_relocation, catalina: "862cc79c1df464dabd3c95ae83d64e2921a0e1f96f561dd958cfd498ef2bd656"
   end
 
   depends_on xcode: :build
