75632aa4d5ac603964500ab9457e2943a45be806
WingLim <643089849@qq.com>
2021-05-21 18:15:25 +0800

    micfix 1.2.0
    * micfix 1.2.0
    * fix: use swift to build
    
    Closes #4.
    
    Signed-off-by: LimxBot <84507948+LimxBot@users.noreply.github.com>

--- Formula/micfix.rb
+++ Formula/micfix.rb
@@ -2,8 +2,8 @@ class Micfix < Formula
   desc "Fix Headset/Headphone Micphone in Hackintosh with AppleALC"
   homepage "https://github.com/WingLim/MicFix"
   url "https://github.com/WingLim/MicFix.git",
-      tag:      "v1.1.3",
-      revision: "9337514ef3aee3e320bcce56de4985a474daf715"
+      tag:      "v1.2.0",
+      revision: "81a73926e549dab7461d9f76d200a6f3a98e82b2"
   license "MIT"
 
   bottle do
@@ -14,8 +14,8 @@ class Micfix < Formula
   depends_on xcode: :build
 
   def install
-    xcodebuild "-configuration", "Release", "SYMROOT=build"
-    bin.install "build/Release/MicFix"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/MicFix"
   end
 
   plist_options manual: "MicFix"
